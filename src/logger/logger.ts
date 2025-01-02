7e01042c625cef24cf105b3fe51d171456e2b9b1
Alberto Moretti <moretti919@gmail.com>
2025-01-02 16:49:57 +0100

    feat: add prefix on log

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -9,29 +9,30 @@ export const LogLevel = {
 export type LogLevelType = (typeof LogLevel)[keyof typeof LogLevel];
 
 let globalLogLevel: LogLevelType = LogLevel.SILENT;
+const logPrefix = "[syncinator]";
 
 export class Logger {
 	debug(message: unknown, ...optionalParams: unknown[]) {
 		if (globalLogLevel > LogLevel.SILENT && globalLogLevel <= LogLevel.DEBUG) {
-			console.debug(message, ...optionalParams);
+			console.debug(logPrefix, message, ...optionalParams);
 		}
 	}
 
 	info(message: unknown, ...optionalParams: unknown[]) {
 		if (globalLogLevel > LogLevel.SILENT && globalLogLevel <= LogLevel.INFO) {
-			console.info(message, ...optionalParams);
+			console.info(logPrefix, message, ...optionalParams);
 		}
 	}
 
 	warn(message: unknown, ...optionalParams: unknown[]) {
 		if (globalLogLevel > LogLevel.SILENT && globalLogLevel <= LogLevel.WARN) {
-			console.warn(message, ...optionalParams);
+			console.warn(logPrefix, message, ...optionalParams);
 		}
 	}
 
 	error(message: unknown, ...optionalParams: unknown[]) {
 		if (globalLogLevel > LogLevel.SILENT && globalLogLevel <= LogLevel.ERROR) {
-			console.error(message, ...optionalParams);
+			console.error(logPrefix, message, ...optionalParams);
 		}
 	}
 	setGlobalLevel(level: LogLevelType) {
