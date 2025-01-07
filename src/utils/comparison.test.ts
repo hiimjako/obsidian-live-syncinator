95b71a18383b3799539471ddc5e42a998119ca6b
Alberto Moretti <moretti919@gmail.com>
2025-01-07 21:31:30 +0100

    test: fix comparison test

--- src/utils/comparison.test.ts
+++ src/utils/comparison.test.ts
@@ -4,18 +4,15 @@ import { shallowEqualStrict } from "./comparison";
 
 describe("shallowEqualStrict", () => {
 	test("equal objects", () => {
-		const obj1 = [
-			{
-				foo: 1,
-				bar: "foo",
-			},
-		];
-		const obj2 = [
-			{
-				bar: "foo",
-				foo: 1,
-			},
-		];
+		const obj1 = {
+			foo: 1,
+			bar: "foo",
+		};
+
+		const obj2 = {
+			bar: "foo",
+			foo: 1,
+		};
 
 		assert.equal(shallowEqualStrict(obj1, obj2), true);
 	});
